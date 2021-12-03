71a0d7da33f9aa4c43bedd75a4325540c2aa04a9
Sebosun <sebos@protonmail.com>
2021-12-03 01:36:04 +0100

    pulling out the styles for the main tag out of the child component into the parent one

--- src/components/game/options/GameOptions.tsx
+++ src/components/game/options/GameOptions.tsx
@@ -56,7 +56,7 @@ export default function GameOptions(): ReactElement | null {
   const nounKeys: string[] = Object.keys(parsed.nouns);
 
   return (
-    <main className="w-full max-h-96 overflow-y-auto bg-gray-100 rounded-2xl shadow-l text-gray-800 p-4">
+    <>
       <p className="text-xl mx-4 leading-loose">Nouns:</p>
       <div className="lg:flex  gap-5 text-2xl justify-center">
         {nounKeys.map((item) => {
@@ -72,6 +72,6 @@ export default function GameOptions(): ReactElement | null {
           );
         })}
       </div>
-    </main>
+    </>
   );
 }
--- src/components/pages/Options.tsx
+++ src/components/pages/Options.tsx
@@ -2,5 +2,9 @@ import React, { ReactElement } from "react";
 import GameOptions from "../game/options/GameOptions";
 
 export default function Options(): ReactElement | null {
-  return <GameOptions />;
+  return (
+    <main className="w-full max-h-96 overflow-y-auto bg-gray-100 rounded-2xl shadow-l text-gray-800 p-4">
+      <GameOptions />;
+    </main>
+  );
 }
